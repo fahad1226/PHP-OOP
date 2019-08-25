897a5ca0b3703a9f207aaefe7bd56f8e57ec276b
Fahad <fahadbinmnr@gmail.com>
2019-08-25 21:50:21 +0600

    add another student object

--- simpleClass.php
+++ simpleClass.php
@@ -18,15 +18,25 @@
             echo "university name is ".$this->university;
             echo "<br>";
             echo "cgpa is ".$this->cgpa;
+            echo "<br>";
+            echo "<br>";
         }
 
    }
 
-   $student1 = new Student();
+   $student1 = new Student(); //object created for first student 
 
    $student1->name = "Fahad Bin Munir";
    $student1->age = 21;
    $student1->cgpa = 3.64;
    $student1->Behaviour();
+
+   
+   $student2 = new Student(); //object created for second student
+
+   $student2->name = "Rasik Hawk";
+   $student2->age = 21;
+   $student2->cgpa = 3.10;
+   $student2->Behaviour();
 
 ?>
