c7e362dcf077be37d79cd67c79471e28e2168c84
Fahad <fahadbinmnr@gmail.com>
2019-08-26 16:57:42 +0600

    added spl_autoload_register method,which is most populer

--- index.php
+++ index.php
@@ -7,10 +7,14 @@
 
 
 
-    function __autoload($classname) {    // autoload function to load class without including all single files.
-        echo "$classname";
+    // function __autoload($classname) {    // autoload function to load class without including all single files.
+    //     echo "$classname";
+    //     include "Classes/".$classname.".php";   --->> One way
+    // }
+
+    spl_autoload_register(function($classname){   // -->> most populer way to auto load classes
         include "Classes/".$classname.".php";
-    }
+    });
 
     $js = new JavaScript();
     $java = new java();
